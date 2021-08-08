09f7de0f1915d221c5b92fc9cc64f8553265a7e1
fmy <fmyxyz@163.com>
2021-08-08 12:29:55 +0800

    NewClient

---
+++ xredis.go
@@ -0,0 +1,53 @@
+package redis
+
+import (
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// NewClient returns a client to the Redis Server specified by Options.
+func NewClient(opt *Options) *Client {
+	if opt.Tag == "" {
+		opt.Tag = "json"
+	}
+	if opt.Expiration == 0 {
+		opt.Expiration = -1
+	}
+	client := redis.NewClient(&opt.Options)
+	return &Client{Client: client, options: *opt}
+}
+
+type Client struct {
+	*redis.Client
+	options Options
+}
+
+// Options keeps the settings to setup redis connection.
+type Options struct {
+	redis.Options
+	Tag         string
+	Expiration  time.Duration
+	Start, Stop int64
+}
+
+type Option func(opt *Options)
+
+func Tag(tag string) Option {
+	return func(opt *Options) {
+		opt.Tag = tag
+	}
+}
+
+func Expiration(expiration time.Duration) Option {
+	return func(opt *Options) {
+		opt.Expiration = expiration
+	}
+}
+
+func Range(start, stop int64) Option {
+	return func(opt *Options) {
+		opt.Start = start
+		opt.Stop = stop
+	}
+}
